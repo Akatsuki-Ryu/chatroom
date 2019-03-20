4717ceb6962ca8741a93c8fbeb19b5608ce2abcb
akatsuki <AkafreakingBox>
2019-03-20 23:36:20 +0200

    serverside message handling frontend

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+// const io = require("socket.io")(server);
+let rooms = [];
+let messages = [];
+let idCounter = 0;
+
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// router.get('/', function(req, res, next) {
+//   // res.render('index', { title: 'Express' });
+//   res.send("hello world");
+// });
+
+
+router.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
 });
 
+
+
 module.exports = router;
+
+
