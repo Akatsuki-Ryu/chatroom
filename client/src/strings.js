16490eeb39c85b4742509dd0ab120b39fdb89c13
akatsuki <AkafreakingBox>
2019-03-25 01:33:23 +0200

    updatig strings

--- client/src/strings.js
+++ client/src/strings.js
@@ -1,5 +1,9 @@
+
 module.exports = {
+    HEADER: "Chatbox",
+    LOGIN: "Login",
     ROOMS_HEADER: "Rooms",
     SUBMIT: "Submit",
+    SEND: "Send",
     USERNAME_LABEL: "What's your nickname?   "
 }
