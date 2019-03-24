9e14864bde4d9883070655e49ca02fa787ebebe2
akatsuki <AkafreakingBox>
2019-03-25 01:26:12 +0200

    rooms

--- client/src/components/RoomList.js
+++ client/src/components/RoomList.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { List } from "semantic-ui-react";
 
-const RoomList = ({header, data, onItemClick}) => (
-    <ul>
-        {data && data.map(item => (
-            <li key={item.id} onClick={() => onItemClick(item.id)}>{item.name}</li>
-        ))}
-    </ul>
+const RoomList = ({ header, data, selectedId, onItemClick }) => (
+    <List selection>
+        { data && data.map(item => (
+            <List.Item key={ item.id } active={ item.id === selectedId } onClick={ () => onItemClick(item.id) }>{ item.name }</List.Item>
+        )) }
+    </List>
 )
 
 export default RoomList
