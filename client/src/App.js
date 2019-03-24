7b68481785f1b736a95213ec606db9d180782d18
akatsuki <AkafreakingBox>
2019-03-24 19:06:57 +0200

    message init

--- client/src/App.js
+++ client/src/App.js
@@ -23,8 +23,27 @@ import * as strings from "./strings";
 
 
 class App extends Component {
+
+
+  componentDidMount() {
+    // Connect socket
+    let socket = io("http://localhost:3000");
+    socket.on(constants.SOCKET_CONNECT, () => this.props.onSocketConnect(socket));
+    socket.on(constants.ROOM_RECEIVE, this.props.onRoomReceive);
+    socket.on(constants.MESSAGE_RECEIVE, this.props.onMessageReceive);
+  }
+
+  onMessageSend = messageText => {
+    // Send new message to the server
+    this.props.socket.emit(constants.MESSAGE_SEND, { text: messageText, username: this.props.username, roomId: this.props.roomId });
+  }
+
+
   render() {
     return (
+
+
+
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
