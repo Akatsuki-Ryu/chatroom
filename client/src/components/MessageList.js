3478ccee1b5897e38436ec70dac8e874eba60bef
akatsuki <AkafreakingBox>
2019-03-25 20:00:45 +0200

    visuals of the chat bubble

--- client/src/components/MessageList.js
+++ client/src/components/MessageList.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { List, Segment } from "semantic-ui-react";
+import {List, Segment} from "semantic-ui-react";
 
-const MessageList = ({ data, username }) => {
+const MessageList = ({data, username}) => {
     const formatDate = date => {
         const pad = n => n < 10 ? "0" + n : n;
         return pad(date.getHours()) + ":" + pad(date.getMinutes());
@@ -11,18 +11,24 @@ const MessageList = ({ data, username }) => {
     const getColor = (username1, username2) => username1 === username2 ? "green" : "blue"
 
     return (
-        <List>
-            { data && data.map(message => (
+        <List relaxed>
+            {data && data.map(message => (
                 <List.Item key={message.id}>
-                    <Segment floated={ getAlignment(username, message.username) } inverted color={ getColor(username, message.username) } style={{ textAlign:getAlignment(username, message.username) }}>
+                    <Segment padded
+                             floated={getAlignment(username, message.username)} inverted
+                             color={getColor(username, message.username)}
+                             style={{textAlign: getAlignment(username, message.username)}}>
                         <p>
-                            <span style={{ fontWeight:"bold" }}>{ message.username }</span><br/>
-                            <span style={{ fontSize:"medium" }}>{ message.text }</span><br/>
-                            <span style={{ fontSize:"mini", color:"grey" }}>{ formatDate(new Date(message.timestamp)) }</span>
+                            <span style={{fontWeight: "bold"}}>{message.username}</span><br/>
+                            <span style={{fontSize: "medium"}}>{message.text}</span><br/>
+                            <span style={{
+                                fontSize: "mini",
+                                color: "grey"
+                            }}>{formatDate(new Date(message.timestamp))}</span>
                         </p>
                     </Segment>
                 </List.Item>
-            )) }
+            ))}
         </List>
     )
 }
